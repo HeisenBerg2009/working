f5540d9867f54aca104169f0a7955cc8f8432463
HeisenBerg2009 <lukamircxulava23@gmail.com>
2026-07-03 14:50:51 +0400

    Add files via upload

---
+++ newproject/assets/js/script.js
@@ -0,0 +1,45 @@
+let mozaidze = {
+  name: "alex",
+  lastName: "moza",
+  age: 94,
+  country: "georgia",
+  isStudent: true,
+};
+
+let mircxu = {
+  name: "luka",
+  lastName: "mircxulava",
+  age: 27,
+  country: "samegrelo",
+  isStudent: true,
+};
+
+// if (age === 94) {
+//   alert("თქვენი ადგილი აქ არ მოიძბნება!!!");
+// }
+
+// if (age < 16) {
+//   alert("აქ რაგინდა, წადი დაიძინე ბლიადდდ!!!");
+// }
+
+// if (age > 16) {
+//   alert(
+//     "მობრძანდი და დადე ტრაკი აქ და იმუშავე, თორემ პიზდეცს დაგმართებთ, არა? მაშინ დაგენძრაააა!!!",
+//   );
+// }
+
+// if (name === "alex") {
+//   alert("an ari da an arari");
+// }
+
+// if (lastName === "moza") {
+//   alert("saqme saqmea bliad!!!");
+// }
+
+if (mircxu.isStudent && mircxu.name === "luka") {
+  alert("😂😂😂😂😂😂😂");
+}
+
+if (mozaidze.isStudent && mozaidze.name === "alex") {
+  alert("es biwi ar dadis kolejshi xshirad");
+}
